a7cc8d84744ce429f732e6b68b8beb7ed3a5bf4c
RodrigoMarques23 <rodrigomarques6923@gmail.com>
2022-03-08 14:28:58 +0000

    Add Bye World

--- Olamundo/Program.cs
+++ Olamundo/Program.cs
@@ -7,6 +7,7 @@ namespace Olamundo
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+            Console.WriteLine("Bye World!");
         }
     }
 }
