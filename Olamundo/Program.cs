bd877ce0df408d999e29706237328d82d94ae417
Butchsss <rodrigomarques23work@gmail.com>
2022-03-14 14:59:48 +0000

    Comentarios

--- Olamundo/Program.cs
+++ Olamundo/Program.cs
@@ -2,8 +2,15 @@
 
 namespace Olamundo
 {
+    /// <summary>
+    /// Projeto Olamundo 
+    /// </summary>
     class Program
     {
+        /// <summary>
+        /// Função main, onde o programa começa
+        /// </summary>
+        /// <param name="args"></param>
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
