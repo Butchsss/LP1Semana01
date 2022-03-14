53411ec6b3a44ec76ce96515d6ef3b3672c8e66c
Butchsss <rodrigomarques23work@gmail.com>
2022-03-14 15:13:49 +0000

    char added

--- TiposVarios/Program.cs
+++ TiposVarios/Program.cs
@@ -6,14 +6,18 @@ namespace TiposVarios
     {
         static void Main(string[] args)
         {
-            int i = 3;
+            //int i = 3;
             int k = 0x10;
-            uint l = 0x10U;
+            //uint l = 0x10U;
             int x = 0xFF_2B_4A;
             int y = 0xFF2B4A;
+            char a = '\u00A9';
+            char b = '\u00B6';
             Console.WriteLine(k);
             Console.WriteLine(y);
             Console.WriteLine(x);
+            Console.WriteLine(a);
+            Console.WriteLine(b);
 
         }
     }
