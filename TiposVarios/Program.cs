4ac9de6ce3db13e9a420c3267e5b8ff4baf53610
Butchsss <rodrigomarques23work@gmail.com>
2022-03-14 15:08:50 +0000

    inteiros adicionados

---
+++ TiposVarios/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace TiposVarios
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int i = 3;
+            int k = 0x10;
+            uint l = 0x10U;
+            int x = 0xFF_2B_4A;
+            int y = 0xFF2B4A;
+            Console.WriteLine(k);
+            Console.WriteLine(y);
+            Console.WriteLine(x);
+
+        }
+    }
+}
