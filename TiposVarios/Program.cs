7060c93675cd46a9c8acafdf7f2a3cfaaa1e6a45
Butchsss <rodrigomarques23work@gmail.com>
2022-03-15 01:02:43 +0000

    Ex03 Finished

--- TiposVarios/Program.cs
+++ TiposVarios/Program.cs
@@ -6,14 +6,22 @@ namespace TiposVarios
     {
         static void Main(string[] args)
         {
-            //int i = 3;
             int k = 0x10;
-            //uint l = 0x10U;
             int x = 0xFF_2B_4A;
             int y = 0xFF2B4A;
             char a = '\u00A9';
             char b = '\u00B6';
+            float f = 5.14f;
+            double d = 4000.480;
+            decimal m = 0.1452M;
+            bool condition = false;
             Console.WriteLine($"Int K ={k}  Int Y = {y}  Int X = {x}  Char A = {a} Char B = {b}");
+            Console.WriteLine($"Float f ={f} Double = {d} Decimal {m} Bool = {condition}");
+            if (f < d)
+            {
+                condition = true;
+                Console.WriteLine(condition);
+            }
 
         }
     }
