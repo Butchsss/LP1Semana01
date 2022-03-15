5475a5803ee6b3e5caec5a9942e0e5691782aade
Butchsss <rodrigomarques23work@gmail.com>
2022-03-15 01:25:49 +0000

    Ex05 Finished

--- VariasStrings/Program.cs
+++ VariasStrings/Program.cs
@@ -6,10 +6,22 @@ namespace VariasStrings
     {
         static void Main(string[] args)
         {
+            int x = 45;
+            int i = 55;
             string s = " tab\t e um copyright \u03A9  ";
             string d = @"""Esta string esta entre aspas""  ";
             string c = @"Nao e' um codigo Unicode \1234, nao e' nova linha \n  ";
             Console.WriteLine($" S{s} D{d} C{c}");
+            string a = "ola + " + 3;
+            string b = " = ola + 3";
+            string e = "tudo bem?" + b;
+            string f = String.Format("Arg {0} e {1}", x, i);
+            Console.WriteLine($"{a}{b}{e}");
+            Console.WriteLine(f);
+            Console.WriteLine($"valor de x = {x}");
+            Console.WriteLine($"Valor de  {i} + {x} = { i + x }");
+
+
         }
     }
 }
