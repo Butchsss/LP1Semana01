79f8f03edfe2756db2e4525504a0786c5b0513b1
Butchsss <rodrigomarques23work@gmail.com>
2022-03-15 01:11:59 +0000

    First Step

---
+++ VariasStrings/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace VariasStrings
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string s = " tab\t e um copyright \u03A9  ";
+            string d = @"""Esta string esta entre aspas""  ";
+            string c = @"Nao e' um codigo Unicode \1234, nao e' nova linha \n  ";
+            Console.WriteLine($" S{s} D{d} C{c}");
+        }
+    }
+}
